43e6c66e59c8b6760452403d9c0e5ce55e7f063e
zoldyck04killu <killua04.zoldyck@gmail.com>
2018-12-05 07:26:46 +0000

    pengembalian test coba gan

--- view/admin/transaksi/daftar-peminjam.php
+++ view/admin/transaksi/daftar-peminjam.php
@@ -30,7 +30,33 @@
       $no = 1;
       // $b = $data->fetch_array();
       while ($a = $data->fetch_array()) {
-        if (strtotime(date('Y-m-d')) > strtotime($a['tgl_kembali']) ) {
+
+        $tgl1 = $a['tgl_pinjam'];
+        $tgl2 = $a['tgl_kembali'];
+
+// pecah menjadi angka $tgl1
+        $pecah1 = explode("-", $tgl1);
+        $date1 = $pecah1[2];
+        $month1 = $pecah1[1];
+        $year1 = $pecah1[0];
+//end
+
+// pecah menjadi angka $tgl2
+        $pecah2 = explode("-", $tgl2);
+        $date2 = $pecah2[2];
+        $month2 = $pecah2[1];
+        $year2 = $pecah2[0];
+//end
+
+// hitung selisih tanggal
+        $jd1 = GregorianToJD($month1, $date1, $year1);
+        $jd2 = GregorianToJD($month2, $date2, $year2);
+        $selisih = $jd2 - $jd1;
+// end
+
+
+// var_dump($selisih);
+        if (($selisih-8)<=0) {
           // $tgl_skarang = date('d');
           // // echo -$tgl_skarang;
           // $tgl_kembali = $a['tgl_kembali'];
@@ -40,17 +66,21 @@
           // // $tgl2 = date('Y-m-d', strtotime('-5 days', strtotime($tgl_kembali))); //operasi penjumlahan tanggal sebanyak 6 hari
           // echo $tgl2; //print tanggal
 
-          $tgl_skarang = date('d');
-          $tgl_kembali = $a['tgl_kembali'];
-          $day = substr($tgl_kembali,8,2);
-          $jlh_denda = (int)$tgl_skarang - (int)$day;
-          $hargaDenda = abs($jlh_denda) * 5000;
-          $denda = "Rp. ".number_format($hargaDenda, 0, ".", ".");
+          // $tgl_skarang = date('d');
+          // $tgl_kembali = $a['tgl_kembali'];
+          // $day = substr($tgl_kembali,8,2);
+          // $jlh_denda = (int)$tgl_skarang - (int)$day;
+          // $hargaDenda = abs($jlh_denda) * 5000;
+          // $denda = "Rp. ".number_format($hargaDenda, 0, ".", ".");
           // echo $jlh_denda;
           // die('denda');
-        }else {
-          // die('tidak');
+
           $denda = "Tidak Denda";
+        }else {
+          // ganti angka 8 dengan telat hari pengembalian
+          $bayar = ($selisih-8)*5000;
+          // end
+          $denda = "Rp. ".number_format($bayar, 0, ".", ".");
         }
       ?>
       <tr>
